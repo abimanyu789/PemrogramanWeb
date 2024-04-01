d16fd91463957f7287395a6610f0188be4e88e3c
abimanyu789 <anandaabimanyu7@gmail.com>
2024-04-02 04:54:02 +0700

    JS6-Praktikum6 Menu Bertingkat

---
+++ Pertemuan6/praktik_php/array_menuBertingkat.php
@@ -0,0 +1,52 @@
+<?php
+// membuat array dari menu
+$menu = [
+    [ 
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita", 
+        "subMenu" => 
+        [ 
+            [ 
+                "nama" => "Wisata",
+                "subMenu" => 
+                [
+                    [ 
+                        "nama" => "Pantai"
+                    ],
+                    [ 
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [ 
+                "nama" => "Kuliner"
+            ],
+            [ 
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+        [
+            "nama" => "Tentang"
+        ],
+        [ 
+            "nama" => "Kontak"
+        ],
+];
+// membuat fungsi menampilkan menu
+function tampilkanMenuBertingkat (array $menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $item) { 
+        echo "<li>{$item['nama']}";
+        if (isset($item['subMenu']) && is_array($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']); // Rekursif
+        }
+        echo "</li>";
+    } 
+    echo "</ul>";
+}
+// Memanggil fungsi
+tampilkanMenuBertingkat($menu);
+?>
