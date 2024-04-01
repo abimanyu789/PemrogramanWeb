0beefe9d861788f2c79b4fa38c1855c617e58a63
abimanyu789 <anandaabimanyu7@gmail.com>
2024-04-02 05:03:03 +0700

    JS6-Praktikum12 Variabel Superglobal

---
+++ Pertemuan6/praktik_php/halo_dunia.php
@@ -0,0 +1,16 @@
+<?php
+// Mengambil nilai parameter 'nama' dari URL, dengan menggunakan '@' untuk menekan peringatan jika key-nya kosong
+$nama = @$_GET['nama'];
+
+// Mengambil nilai parameter 'usia' dari URL, dengan menggunakan '@' untuk menekan peringatan jika key-nya kosong
+$usia = @$_GET['usia'];
+
+// Menampilkan pesan sapaan dengan menggunakan variabel $nama dan $usia
+echo "Halo {$nama}! Apakah benar Anda berusia {$usia} tahun?";
+?>
+
+<<<<<<< HEAD
+// code dari variabelget
+=======
+// code dari variabelget
+>>>>>>> c336d02523c00bc78343fd17c62c459c4383e899
